8ab7b47fbe932fb1528cf0abcfaeb71fa431249a
EMEAAD\rmanfellotto <riccardo.manfellotto@soprasteria.com>
2025-07-31 12:01:45 +0200

    modificati i controller

--- src/main/java/com/project/toDoList/controller/TaskController.java
+++ src/main/java/com/project/toDoList/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.project.toDoList.controller;
 
 import com.project.toDoList.dto.TaskDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,14 +27,18 @@ public class TaskController {
 
 
 
-    //Restituisce tutte le task
+    @Operation (description = "Restituisce tutte le task", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "200", content = @Content (mediaType = "application/json"))
+    })
     @GetMapping
     public ResponseEntity<List<TaskDTO>> getAllTasks () {
 
         return ResponseEntity.ok(taskService.getTasks());
     }
 
-    //Restituisce tutte le task di un User
+    @Operation (description = "Restituisce tutte le task di un utente", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "200", content = @Content (mediaType = "application/json"))
+    })
     @GetMapping({"/user/{userId}"})
     public ResponseEntity<List<TaskDTO>> getAllUserTasks (@PathVariable Long userId){
 
@@ -40,13 +47,17 @@ public class TaskController {
     }
 
 
-    //Restituisce le task tramite id
+    @Operation (description = "Restituisce una task per ID", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "200", content = @Content (mediaType = "application/json"))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.getTaskById(id));
     }
 
-    //Crea Task
+    @Operation (description = "Crea una nuova task", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "201", content = @Content (mediaType = "application/json"))
+    })
     @PostMapping
     public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
 
@@ -55,14 +66,18 @@ public class TaskController {
                 .body(taskService.createTask(taskDTO));
     }
 
-    //Aggiorno una task
+    @Operation (description = "Aggiorna una task esistente", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "200", content = @Content (mediaType = "application/json"))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<TaskDTO> updateTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
             return ResponseEntity.ok(taskService.updateTask(id, taskDTO));
 
     }
 
-    //Cancello una task
+    @Operation (description = "Elimina una task per ID", responses = {
+        @ApiResponse (description = "Successful Operation", responseCode = "200", content = @Content (mediaType = "application/json"))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<TaskDTO> deleteTask(@PathVariable Long id) {
         return ResponseEntity.ok(taskService.deleteTask(id));
--- src/main/java/com/project/toDoList/controller/UserController.java
+++ src/main/java/com/project/toDoList/controller/UserController.java
@@ -4,6 +4,8 @@ import com.project.toDoList.dto.UserCreateWithPasswordDTO;
 import com.project.toDoList.dto.UserDTO;
 import com.project.toDoList.model.User;
 import com.project.toDoList.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,27 +26,41 @@ public class UserController {
 
 
 
-
+    @Operation (description = "Restituisce tutti gli utenti", responses = {
+            @ApiResponse(description = "Operazione riuscita", responseCode = "200")
+    })
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
 }
 
+    @Operation (description = "Restituisce un utente per ID", responses = {
+            @ApiResponse(description = "Operazione riuscita", responseCode = "200")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUserById(Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
+    @Operation (description = "Crea un nuovo utente", responses = {
+            @ApiResponse(description = "Operazione riuscita", responseCode = "201")
+    })
     @PostMapping
     public ResponseEntity<UserCreateWithPasswordDTO> createUser(UserCreateWithPasswordDTO userDTO) {
         return ResponseEntity.ok(userService.createUser(userDTO));
     }
 
+    @Operation (description = "Aggiorna un utente esistente", responses = {
+            @ApiResponse(description = "Operazione riuscita", responseCode = "200")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
         return ResponseEntity.ok(userService.updateUser(id, userDTO));
     }
 
+    @Operation (description = "Elimina un utente per ID", responses = {
+            @ApiResponse(description = "Operazione riuscita", responseCode = "204")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
